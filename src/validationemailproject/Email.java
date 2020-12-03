5c57e37fb2e79aa0100b434d17680ea5730de578
Rahul Reddy <k.rahul2604@gmail.com>
2020-12-03 09:39:43 +0530

    Update Email.java
    String Object creations: No need of new String creation.

--- src/validationemailproject/Email.java
+++ src/validationemailproject/Email.java
@@ -8,56 +8,57 @@ import java.util.regex.Pattern;
 
 public class Email {
 
-	private String emailAddress;
-	
-	
-	public String getEmailAddress() {
-		return emailAddress;
-	}
+    private String emailAddress;
 
-	public void setEmailAddress(String emailAddress) {
-		this.emailAddress = emailAddress;
-	}
-	
-	public Email(String emailAddress) {
-		super();
-		this.emailAddress = emailAddress;
-	}
-	
 
-	@Override
-	public String toString() {
-		return emailAddress;
-	}
-	
-	public static final String regex = "^([A-Za-z0-9\\-_.]+)@(.+)$";
+    public String getEmailAddress() {
+        return emailAddress;
+    }
 
-	public static void main(String[] args) {
-		List<Email> emailList = new ArrayList<Email>();		
-		emailList.add(new Email("altafmquadri@gmail.com"));
-		emailList.add(new Email("ozhorov@gmail.com"));
-		emailList.add(new Email("ozhorov@gmail.com"));
-		
-		Pattern pattern = Pattern.compile(regex);
-		Scanner scanner = new Scanner(System.in);
-		
-		int counter = 0;
-		
-		while (counter != 1) {
-			System.out.println("Please enter an email address: ");
-			
-			String emailAddress = scanner.nextLine().toLowerCase();
-			Matcher matcher = pattern.matcher(emailAddress);
-			String valid = (matcher.matches() ? "valid" : "invalid");
-			
-			System.out.println("\nThe email is " + emailAddress + " is "+ valid + "\n");
-			
-			if (valid == "valid")
-				System.out.println(emailList.toString().contains(emailAddress) ? "The email address exists." : "Email address is not found!");	
-			
-			System.out.println("\nWould you like to search again? y/n");
-			String continued = scanner.nextLine();
-			if (continued.equals("n")) counter++;
-		}
-	}
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public Email(String emailAddress) {
+        super();
+        this.emailAddress = emailAddress;
+    }
+
+
+    @Override
+    public String toString() {
+        return emailAddress;
+    }
+
+    public static final String regex = "^([A-Za-z0-9\\-_.]+)@(.+)$";
+
+    public static void main(String[] args) {
+        List<Email> emailList = new ArrayList<Solution>();
+        emailList.add(new Email("altafmquadri@gmail.com"));
+        emailList.add(new Email("ozhorov@gmail.com"));
+        emailList.add(new Email("ozhorov@gmail.com"));
+        emailList.add(new Email("k.rahul2604@gmail.com"));
+
+        Pattern pattern = Pattern.compile(regex);
+        Scanner scanner = new Scanner(System.in);
+
+        int counter = 0;
+
+       do {
+           System.out.println("Please enter an email address: ");
+
+           String emailAddress = scanner.nextLine().toLowerCase();
+           Matcher matcher = pattern.matcher(emailAddress);
+
+           if (matcher.matches()) {
+               System.out.println("\nThe email is " + emailAddress + " is valid");
+               System.out.println(emailList.toString().contains(emailAddress) ? "The email address exists." : "Email address is not found!");
+           } else {
+               System.out.println("\nThe email is " + emailAddress + " is invalid");
+           }
+           System.out.println("\nWould you like to search again? y/n");
+           String continued = scanner.nextLine();
+           if (continued.equals("n")) counter++;
+       } while (counter != 1);
+    }
 }
